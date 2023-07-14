02edb1e5b41b83a2aad23e049ddb8b7f39f85b4d
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-07-14 12:26:57 -0300

    transformando os pokemons em li

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,15 +26,18 @@ function convertPokemonToLi(pokemon) {
 }
 
 
+const pokemonList = document.getElementById('pokemonList')
 
 
 fetch(url)
 .then((response) => response.json())
 .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-        for (let i = 0; i < pokemonList.length; i++) {
-            const pokemon = pokemonList[i];
-            console.log(convertPokemonToLi(pokemon));
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i]
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            
+            
         }
         
 
