eee23b3da0c7a21597fbf0448e6f423f0dc138c6
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-08-02 17:47:44 -0300

    separando consumo da API da manipulação HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-const offset = 0
-
-const limit = 10
-
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
 function convertPokemonToLi(pokemon) {
     return `<li class="pokemon">
@@ -29,10 +24,9 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList')
 
 
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
+
+
+pokeAPI.getPokemons().then((pokemons) => {
         for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i]
             pokemonList.innerHTML += convertPokemonToLi(pokemon)
