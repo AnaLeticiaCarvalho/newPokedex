0b092721e0334a29fe72c2d9841b3dd0ac08584f
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-08-19 11:25:01 -0300

    criei um botão que tem a função de carregar mais pokemons e troquei algumas cores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,30 @@
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const limit = 5;
+let offset = 0;
 
-
-
-
-
-function convertPokemonToLi(pokemon) {
-    return `<li class="pokemon ${pokemon.type}">
-
-    <span class="number">#${pokemon.number}</span>
-    <span class="name">${pokemon.name}</span>
-
-    <div class="details">
-
-        <ol class="types">
-        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-
-        </ol>
-        <img src="${pokemon.photo}" 
-         alt="${pokemon.name}">
-    </div>
-
-  
-</li>`
+function loadPokemonItems(offset, limit) {
+    pokeAPI.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="details">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('');
+        pokemonList.innerHTML += newHtml;
+    });
 }
 
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    loadPokemonItems(offset, limit);
+});
 
-const pokemonList = document.getElementById('pokemonList')
-
-
-
-
-pokeAPI.getPokemons().then((pokemons = []) => {
-
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-
-   })
-    .catch( (error) => console.error(error))
-    
-       
-            
-            
-    
-
-
-   
-
-    
+// Initial load
+loadPokemonItems(offset, limit);
